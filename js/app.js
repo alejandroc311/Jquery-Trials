910b2ed1b420b484048f40c26ae4cd994e781b88
alejandroc311 <alejandroc311@gmail.com>
2016-01-27 13:36:18 -0500

    Testing how to insert content to different  types of elements with Jquery.

--- js/app.js
+++ js/app.js
@@ -3,20 +3,24 @@
 var projectPath;
 var $pathContainer;
 var $body = $("body");
-//create function that adds divs and h1 tags
-  //in the h1 print the path to the project folder.
+//create function that adds divs and h1 tags to page
+  //navigate to and h1 print the path to the project folder.
   function printPath(){
-    $pathContainer = $("<div><h1>"+projectPath+"<h1></div>");
+    $pathContainer = $("<div></div>");
+
+    $($pathContainer).append(getProjectPath());
     $pathContainer.appendTo($body);
+
     //access the h1 tags inside the dive in pathContainer
       //once inside put the projectPath variable in the h1
         //make sure you have the project path
   }
 function getProjectPath(){
-  projectPath = "some path";
+  projectPath = "<h1>some path<h1>";
+  return projectPath;
   //work up to master directory
     //get attr of directory
       //in attr get its src for the path.
 }
-getProjectPath();
+
 printPath();
